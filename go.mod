aeea7a3d5893b56a07f6924b159f8b3f01be7e4b
Tyler Bennet <tylerwbennet@gmail.com>
2023-11-25 19:56:32 -0600

    push file changes

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.19
 
 require (
 	github.com/gin-gonic/gin v1.9.1
+	github.com/kbinani/screenshot v0.0.0-20230812210009-b87d31814237
 	github.com/rs/cors/wrapper/gin v0.0.0-20231013084403-73f81b45a644
 )
 
@@ -11,16 +12,17 @@ require (
 	github.com/bytedance/sonic v1.9.1 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
+	github.com/gen2brain/shm v0.0.0-20230802011745-f2460f5984f7 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.14.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
-	github.com/jinzhu/inflection v1.0.0 // indirect
-	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/jezek/xgb v1.1.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
+	github.com/lxn/win v0.0.0-20210218163916-a377121e959e // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
@@ -35,5 +37,4 @@ require (
 	golang.org/x/text v0.13.0 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
-	gorm.io/gorm v1.25.5 // indirect
 )
--- internal/aetherImageCapture/capture.go
+++ internal/aetherImageCapture/capture.go
@@ -1,33 +1,34 @@
 package aetherImageCapture
 
- import (
- 	"fmt"
-	"runtime"
+import (
 	"errors"
- )
+	"fmt"
+	"runtime"
+)
 
- func captureScreen() {
+func captureScreen() {
 	var capture bool = false
 	var err error
 
 	if runtime.GOOS == "windows" {
 		// windows specific code here...
+		capture, err = winScreenshot()
 
 	} else if runtime.GOOS == "darwin" {
-		capture, err = macScreenshot();
-		
+		capture, err = macScreenshot()
+
 	} else if runtime.GOOS == "linux" {
 		//capture
 	} else {
-		err = errors.New("Unable to determine underlying OS. Supported OS Windows, Darwin, Linux/Ubuntu") 
+		err = errors.New("Unable to determine underlying OS. Supported OS Windows, Darwin, Linux/Ubuntu")
 	}
 
-	if (err == nil && capture == true) {
-		result, err := convertScreenshot();
+	if err == nil && capture == true {
+		result, err := convertScreenshot()
 
-		if (result == true && err == nil) {
-			screenValidator, errDelete := deleteScreenshot();
-			if (screenValidator == true && err == nil) {
+		if result == true && err == nil {
+			screenValidator, errDelete := deleteScreenshot()
+			if screenValidator == true && err == nil {
 
 			} else {
 				fmt.Println(fmt.Sprintf("Screenshot failed to capture, check permissions and GOOS runtime. %s", errDelete))
@@ -35,7 +36,7 @@ package aetherImageCapture
 		} else {
 			fmt.Println(fmt.Sprintf("Screenshot failed to capture, check permissions and GOOS runtime. %s", err))
 		}
-		
+
 	} else {
 		fmt.Println(fmt.Sprintf("Screenshot failed to capture, check permissions and GOOS runtime. %s", err))
 	}
